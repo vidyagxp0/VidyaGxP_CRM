239f44049c1391e8fbb0631e0ad33251f9e59461
pankaj15-04 <118167480+pankaj15-04@users.noreply.github.com>
2024-02-17 14:17:52 +0530

    Developed Login page

--- frontend/src/forms/LoginForm.jsx
+++ frontend/src/forms/LoginForm.jsx
@@ -1,56 +1,109 @@
-import React from 'react';
-import { Form, Input, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-import useLanguage from '@/locale/useLanguage';
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 5px;
+  margin-bottom: 10px;
 
-export default function LoginForm() {
-  const translate = useLanguage();
-  return (
-    <>
-      <Form.Item
-        label={translate('email')}
-        name="email"
-        rules={[
-          {
-            required: true,
-          },
-          {
-            type: 'email',
-          },
-        ]}
-      >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder={translate('email')}
-          type="email"
-          size="large"
-        />
-      </Form.Item>
-      <Form.Item
-        label={translate('password')}
-        name="password"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input.Password
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          placeholder={translate('password')}
-          size="large"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>{translate('Remember me')}</Checkbox>
-        </Form.Item>
 
-        <a className="login-form-forgot" href="/forgetpassword">
-          {translate('Forgot password')}
-        </a>
-      </Form.Item>
-    </>
-  );
+label {
+  margin-bottom: 3px;
+  font-size: 16px;
 }
+
+.input-field {
+  padding: 8px;
+  margin-bottom: 5px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  width: 100%;
+  height: 42px;
+}
+
+input[checkbox] {
+  margin-right: 5px;
+}
+
+button {
+  width: 100%;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 10px;
+  &:hover {
+      background-color: #0056b3;
+  }
+}
+
+.error {
+  color: red;
+}
+`
+
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { login } from '@/redux/auth/actions';
+import { selectAuth } from '@/redux/auth/selectors';
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+const LoginForm = () => {
+  const { isLoading, isSuccess } = useSelector(selectAuth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const onFinish = (values) => {
+    dispatch(login({ loginData: values }));
+    navigate('/');
+  };
+
+  // useEffect(() => {
+  //   if (isSuccess) 
+  // }, [isSuccess]);
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
+  return (
+    <Wrapper>
+      <Formik
+        initialValues={{ email: '', password: '' }}
+        validationSchema={validationSchema}
+        onSubmit={onFinish}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+             <label>Email 
+         <span style={{color: 'red'}}>*</span>
+       </label>
+            <div>
+            <Field className='input-field' type="email" name="email" placeholder="Email" />
+            <ErrorMessage className='error' name="email" component="div" />
+            </div>
+            <label>Password 
+         <span style={{color: 'red'}}>*</span>
+       </label>
+       <div>
+            <Field className='input-field' type="password" name="password" placeholder="Password" />
+            <ErrorMessage className='error' name="password" component="div" />
+       </div>
+
+            <button type="submit" disabled={isSubmitting}>
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+
+export default LoginForm;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,32 +1,21 @@
-import { useEffect } from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import {  useSelector } from 'react-redux';
 
 import useLanguage from '@/locale/useLanguage';
 
-import { Form, Button } from 'antd';
+import { Form } from 'antd';
 
-import { login } from '@/redux/auth/actions';
 import { selectAuth } from '@/redux/auth/selectors';
 import LoginForm from '@/forms/LoginForm';
 import Loading from '@/components/Loading';
 import AuthModule from '@/modules/AuthModule';
 
+import React from 'react';
+
+
 const LoginPage = () => {
   const translate = useLanguage();
   const { isLoading, isSuccess } = useSelector(selectAuth);
-  const navigate = useNavigate();
-  // const size = useSize();
-
-  const dispatch = useDispatch();
-  const onFinish = (values) => {
-    dispatch(login({ loginData: values }));
-  };
-
-  useEffect(() => {
-    if (isSuccess) navigate('/');
-  }, [isSuccess]);
 
   const FormContainer = () => {
     return (
@@ -38,19 +27,10 @@ const LoginPage = () => {
           initialValues={{
             remember: true,
           }}
-          onFinish={onFinish}
+          onFinish={()=>{}}
         >
           <LoginForm />
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-              loading={isLoading}
-              size="large"
-            >
-              {translate('Log in')}
-            </Button>
             {translate('Or')} <a href="/register">{translate('register now')}!</a>
           </Form.Item>
         </Form>
@@ -58,7 +38,7 @@ const LoginPage = () => {
     );
   };
 
-  return <AuthModule authContent={<FormContainer />} AUTH_TITLE="Sign in" />;
+  return <AuthModule authContent={<FormContainer />} AUTH_TITLE="Log in" />;
 };
 
 export default LoginPage;
